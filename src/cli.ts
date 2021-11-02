c02c7e5b54a01d5f429067b110177c733c833097
Davit Barbakadze <jayarjo@gmail.com>
2021-11-02 12:54:17 +0400

    cli: put it straight

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 import { camelCase } from 'camel-case';
 import { existsSync } from 'fs';
 import { resolve } from 'path';
-import { PostgisSeeder } from '.';
+import { PostgisSeeder, Seeder } from '.';
 import { invariant } from './utils';
 
 type CliOptions = {
@@ -12,10 +12,12 @@ type CliOptions = {
   password?: string;
   tables: string;
   factoryDir?: string;
+  num?: number;
+  postgis?: boolean;
 };
 
 const [, , ...args] = process.argv;
-const VERSION = require('./package.json').version;
+const VERSION = require(resolve(__dirname, '../package.json')).version;
 
 const parseOptions = (options: string[]): CliOptions =>
   options.reduce((obj: any, opt: string) => {
@@ -27,18 +29,44 @@ const parseOptions = (options: string[]): CliOptions =>
 const showHelp = () => {
   process.stdout.write(
     `seeder v${VERSION}` +
-      '\n' +
-      'Options:\n' +
+      '\nOptions:\n' +
       '  --version        Show version\n' +
       '  --help           Show this help\n' +
       '\n' +
-      'Usage:\n' +
+      "  --host           DB host (default: 'localhost')\n" +
+      "  --port           DB port (default: '5432')\n" +
+      "  --user           DB user (default: 'postgres')\n" +
+      "  --password       DB password (default: 'postgres')\n" +
+      '  --database       DB name\n' +
+      '\n' +
+      '  --tables         Comma-separated list of tables to seed\n' +
+      '  --factory-dir    Directory containing factories per table\n' +
+      "  --num            Number of rows to generate (default: '10')\n" +
+      "  --postgis        Whether to enable PostGIS adapter (default: 'false')\n" +
+      '\nUsage:\n' +
       '  seeder --help\n\n'
   );
 };
 
 const showVersion = () => {
-  process.stdout.write(`seeder v${VERSION}`);
+  process.stdout.write(`seeder v${VERSION}\n`);
+};
+
+const getFactory = (tableName, factoryDir) => {
+  const factoryPath = resolve(process.cwd(), factoryDir, `${tableName}.js`);
+  if (!existsSync(factoryPath)) {
+    console.error(`Factory for ${tableName} cannot be found at ${factoryPath}`);
+    process.exit(1);
+  }
+
+  const factory = require(factoryPath).default;
+  if (typeof factory !== 'function') {
+    console.error(
+      `Factory file should reside in ${factoryDir}, be named after the table (e.g. ${tableName}.js) and export single factory function as default`
+    );
+    process.exit(1);
+  }
+  return factory;
 };
 
 const handleCommand = async ({
@@ -49,41 +77,49 @@ const handleCommand = async ({
   database,
   tables,
   factoryDir,
+  num = 10,
+  postgis = false,
   ...seederOptions
 }: CliOptions) => {
   invariant(database, `You need to specify a DB to connect to (see --database param option)`);
   invariant(tables, `You need to specify tables to seed (see --tables option)`);
   invariant(
-    factoryDir && !existsSync(factoryDir),
+    !factoryDir || existsSync(factoryDir),
     `Directory for factories (${factoryDir}) is invalid or doesn't exist`
   );
 
-  const seeder = new PostgisSeeder({ host, port, user, password, database }, seederOptions);
+  const seeder = new (postgis ? PostgisSeeder : Seeder)(
+    { host, port, user, password, database },
+    { ignoreNullable: false, fixSerials: false, ...seederOptions }
+  );
 
-  const tableNames = tables.split(',');
+  try {
+    const tableNames = tables.split(',');
 
-  if (!factoryDir) {
-    await seeder.seed(tableNames);
-  } else {
-    const data = tableNames.reduce((obj, tableName) => {
-      const factoryPath = resolve(process.cwd(), factoryDir, 'table.js');
-      if (!existsSync(factoryPath)) {
-        console.error(`Factory for ${tableName} cannot be found at ${factoryPath}`);
-        process.exit(1);
+    if (tableNames.length === 1) {
+      if (!factoryDir) {
+        await seeder.seed(tableNames[0], {}, num);
+      } else {
+        await seeder.seed(tableNames[0], getFactory(tableNames[0], factoryDir), num);
       }
-
-      const factory = require(factoryPath).default;
-      if (typeof factory !== 'function') {
-        console.error(
-          `Factory file should reside in ${factoryDir}, be named after the table (e.g. ${tableName}.js) and export single factory function as default`
+    } else {
+      if (!factoryDir) {
+        await seeder.seed(tableNames);
+      } else {
+        const data = tableNames.reduce(
+          (obj, tableName) => ({ ...obj, [tableName]: getFactory(tableName, factoryDir) }),
+          {}
         );
-        process.exit(1);
+        await seeder.seed(data);
       }
+    }
 
-      return { ...obj, [tableName]: factory };
-    }, {});
-
-    await seeder.seed(data);
+    await seeder.close();
+    process.exit(0);
+  } catch (ex) {
+    console.error(`Error: ${ex.message}`);
+    await seeder.close();
+    process.exit(1);
   }
 };
 
